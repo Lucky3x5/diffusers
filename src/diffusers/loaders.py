e27e940e175859acc3bd5b587f54494d9729a9ea
User <user@ex12465879.com>
2023-05-03 01:17:07 +0700

    Fixed casting for CPU in _load_lora_weights

--- src/diffusers/loaders.py
+++ src/diffusers/loaders.py
@@ -918,7 +918,7 @@ class LoraLoaderMixin:
                 # Cast to UNet / Text encoder type
                 weight_up = weight_up.to(dtype)
                 weight_down = weight_down.to(dtype)
-            elif dtype != torch.float32:
+            elif weight_up.dtype != torch.float32:
                 # Cast to float32 (Fix for RuntimeError: "addmm_impl_cpu_" not implemented for 'Half')
                 weight_up = weight_up.to(torch.float32)
                 weight_down = weight_down.to(torch.float32)
